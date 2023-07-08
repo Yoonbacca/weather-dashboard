0c510138c47180be123d3a5b08012972ec262c07
Yoonbacca <yoonjaekim3@gmail.com>
2023-07-07 19:34:11 -0500

    Added basic structure of site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=35606312ec0c5f42d4cad1b6cf5889f1
+
+// First I need to make a UI
+// The UI must have: a navbar, a search bar, a main card, a dashboard with five cards
+
+// Second I need to build out the search bar
+// When a user searches a location: 
+// The main card populates current and future conditions for the day
+// The 5 day forecast populates
+// The search is saved in local storage
